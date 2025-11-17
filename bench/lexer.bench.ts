504423dd1ea19da77b379949d8a614eed606293f
Gabriel Vaquer <brielov@icloud.com>
2025-11-17 17:15:45 -0300

    refactor(bench): DRY lexer benchmark file

--- bench/lexer.bench.ts
+++ bench/lexer.bench.ts
@@ -2,35 +2,28 @@ import { bench, group, run } from 'mitata'
 import { createCursor, nextToken, type Token, TokenKind } from '../src/lexer'
 import { LARGE_SCRIPT, MEDIUM_SCRIPT, SMALL_SCRIPT } from './fixtures'
 
+function tokenize(script: string): readonly Token[] {
+	const cursor = createCursor(script)
+	const tokens: Token[] = []
+	let token = nextToken(cursor)
+	while (token[0] !== TokenKind.Eof) {
+		tokens.push(token)
+		token = nextToken(cursor)
+	}
+	return tokens
+}
+
 group('Lexer', () => {
 	bench('small script', () => {
-		const cursor = createCursor(SMALL_SCRIPT)
-		const tokens: Token[] = []
-		let token = nextToken(cursor)
-		while (token[0] !== TokenKind.Eof) {
-			tokens.push(token)
-			token = nextToken(cursor)
-		}
+		tokenize(SMALL_SCRIPT)
 	})
 
 	bench('medium script', () => {
-		const cursor = createCursor(MEDIUM_SCRIPT)
-		const tokens: Token[] = []
-		let token = nextToken(cursor)
-		while (token[0] !== TokenKind.Eof) {
-			tokens.push(token)
-			token = nextToken(cursor)
-		}
+		tokenize(MEDIUM_SCRIPT)
 	})
 
 	bench('large script', () => {
-		const cursor = createCursor(LARGE_SCRIPT)
-		const tokens: Token[] = []
-		let token = nextToken(cursor)
-		while (token[0] !== TokenKind.Eof) {
-			tokens.push(token)
-			token = nextToken(cursor)
-		}
+		tokenize(LARGE_SCRIPT)
 	})
 })
 
